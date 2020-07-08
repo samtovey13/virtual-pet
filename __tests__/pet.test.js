b38ce52cd9c290e9128c4b5d7ca8e720bce7646f
Sam Tovey <samtovey13@gmail.com>
2020-07-08 11:33:21 +0100

    added hunger and fitness to function growUp

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,10 @@
 const Pet = require('../src/pet');
 
+let pet;
 
-const pet = new Pet('Fido');
-
+beforeEach(() => {
+    pet = new Pet('Fido');
+});
 
 describe ('constructor', () => {
     it ('returns an object', () => {
@@ -11,9 +13,22 @@ describe ('constructor', () => {
     it ('sets the name property', () => {
         expect(pet.name).toBe('Fido');
     });
-    it ('has function growUp that adds 1 to the age', () => {
+});
+
+describe ('growUp', () => {
+    it ('adds 1 to the age', () => {
         expect(pet.age).toBe(0);
         pet.growUp();
         expect(pet.age).toBe(1);
+    });
+    it ('increases hunger by 5', () => {
+        expect(pet.hunger).toBe(0);
+        pet.growUp();
+        expect(pet.hunger).toBe(5);
+    });
+    it('decreases fitness by 3', () => {
+        expect(pet.fitness).toBe(10);
+        pet.growUp();
+        expect(pet.fitness).toBe(7);
     });
 });
